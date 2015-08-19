bddcaa2863c6878dcf49dc795bcbe56e780f49d2
Benjamin Sheffield <benjaminsheffield@live.com>
2015-08-20 02:11:25 +1200

    Initial commit - adding basic home screen buttons

---
+++ AndroidFragrance/AndroidFragrance/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidFragrance
+{
+    [Activity(Label = "AndroidFragrance", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private List<string> fragranceList;
+        private ListView _mListView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            _mListView = FindViewById<ListView>(Resource.Id.mylistView);
+
+            fragranceList = new List<string>();
+            fragranceList.Add("House");
+            fragranceList.Add("Rating");
+            fragranceList.Add("Gender");
+            fragranceList.Add("Price");
+
+            MyListViewAdapter adapter = new MyListViewAdapter(this, fragranceList);
+
+            _mListView.Adapter = adapter; 
+
+        }
+    }
+}
+
